85c3244cf63a940c3bf58bfc6a9115c63413294a
shijiahao <shijiahao@atguigu.com>
2017-11-23 14:31:49 +0800

    333

--- src/main/java/com/atguigu/test/GitTest.java
+++ src/main/java/com/atguigu/test/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("哈哈发福!");
 		System.out.println("新内容GitHub");
-		System.out.println("新内容GitHub");
+		System.out.println("新内容GitHub55555555");
 	}
 }
